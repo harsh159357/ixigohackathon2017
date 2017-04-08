9bfe51d5c32e5442467a06f612132d657e932805
Haimal <haimal@haimals-macbook-pro.local>
2017-04-08 13:32:41 +0530

    Color changed & file Structure made

---
+++ app/src/main/java/ixigo/invincible/takemethere/haimal/SplashScreen.java
@@ -0,0 +1,12 @@
+package ixigo.invincible.takemethere.haimal;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by haimal on 08/04/17.
+ */
+
+public class SplashScreen extends AppCompatActivity {
+
+
+}
---
+++ app/src/main/java/ixigo/invincible/takemethere/haimal/Utils/API.java
@@ -0,0 +1,10 @@
+package ixigo.invincible.takemethere.haimal.Utils;
+
+/**
+ * Created by haimal on 08/04/17.
+ */
+
+public class API {
+
+
+}
